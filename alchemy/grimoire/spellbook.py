c5b7b93e11a4e754bc654ebdf2ddb7e85114384c
krameraad <eliaspowder@gmail.com>
2026-01-23 00:01:05 +0100

    part 4 almost

--- alchemy/grimoire/spellbook.py
+++ alchemy/grimoire/spellbook.py
@@ -1,2 +1,7 @@
 def record_spell(spell_name: str, ingredients: str) -> str:
-    pass
+    from .validator import validate_ingredients
+
+    validity = validate_ingredients(ingredients)
+    if " VALID" in validity:
+        return f"Spell recorded: [{spell_name}] ({validity})"
+    return f"Spell rejected: [{spell_name}] ({validity})"
--- alchemy/grimoire/validator.py
+++ alchemy/grimoire/validator.py
@@ -1,2 +1,14 @@
 def validate_ingredients(ingredients: str) -> str:
-    pass
+    """If the ingredients contain anything other than
+    `fire`, `water`, `earth` or `air`, return:
+    `[ingredients] - INVALID`, otherwise `[ingredients] - VALID`."""
+    ingredients = ingredients.lower().split()
+    for ingredient in ingredients:
+        if "fire" in ingredient \
+          or "water" in ingredient \
+          or "earth" in ingredient \
+          or "air" in ingredient:
+            del ingredient
+    if len(ingredients) > 0:
+        return f"[{ingredients}] - INVALID"
+    return f"[{ingredients}] - VALID"
